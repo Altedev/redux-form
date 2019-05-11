ef480c949c6773e5b73407ebd2de5194652172cf
altedev <altedev@yandex.ru>
2019-05-11 06:36:33 +0300

    create repository

---
+++ precache-manifest.2130ba06664d5f26cf21ea48ffdc6de5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cdc869db4a01e0edb8aa",
+    "url": "/static/js/main.cdc869db.chunk.js"
+  },
+  {
+    "revision": "507bec3bfa6ddf610532",
+    "url": "/static/js/1.507bec3b.chunk.js"
+  },
+  {
+    "revision": "cdc869db4a01e0edb8aa",
+    "url": "/static/css/main.bb865ee1.chunk.css"
+  },
+  {
+    "revision": "b18b3884082270644fb9d81ff82187b5",
+    "url": "/index.html"
+  }
+];
